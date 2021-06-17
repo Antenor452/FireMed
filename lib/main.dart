6bd43b1949369bb1f4909b3617abd302a3bdb911
Antenor <nanaosei452@gmail.com>
2021-06-17 20:50:49 +0000

    root authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:final_year_project_app/pages/dashboard.dart';
 import 'package:final_year_project_app/pages/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -11,8 +13,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Future _getCurrentUser() async {
+    print('started');
+    var user = await FirebaseAuth.instance.currentUser;
+    print(user);
+
+    return User;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Login());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder(
+          future: _getCurrentUser(),
+          builder: (BuildContext context, AsyncSnapshot snaphot) {
+            if (snaphot.hasData) {
+              UserCredential user = snaphot.data;
+              return Dashboard();
+            } else {
+              return Login();
+            }
+          },
+        ));
   }
 }
